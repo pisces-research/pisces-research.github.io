f490786ad8fb9e96776402e046dc509c7ba3b7d7
dmiddleton <david@pisces.nz>
2026-01-04 15:10:56 +1300

    Limit CPU usage, animate on load only

--- constellation.js
+++ constellation.js
@@ -1,34 +1,45 @@
+
+const hasVisited = sessionStorage.getItem('hasVisitedPage');
+
 // Wait for images to load
 window.addEventListener('load', onLoad)
 
 function onLoad() {
-  gsap.to('.star', {
-    opacity: 0,
+  gsap.fromTo('.star', {opacity: 0}, {
+    opacity: (index, target, targets) => target.dataset.bv,
     duration: 'random(3,6)',
-    ease: 'bounce.in',
+    ease: 'bounce.out',
+    repeatRefresh: true,
     stagger: {
-      amount: 1,
+      amount: 3,
       from: 'random',
-      repeat: -1,
+      repeat: 20,
+      yoyo: true,
     },
   })
 
   animateFish()
 
-  gsap
-    .timeline()
-    .fromTo(
-      '#logo-species path',
-      { opacity: 0, x: -700 },
-      { opacity: 0.5, duration: 0.7, stagger: 0.7, x: 0 },
-      '+=1'
-    )
-    .fromTo('#logo-words', { opacity: 0 }, { opacity: 0.5, duration: 3 }, '+=1')
-    .to('#byline-text-intro', { autoAlpha: 1, duration: 3 }, '+=0.2')
-    .to('#logo-species path', { autoAlpha: 1, duration: 3 })
-    .to('#logo-words', { autoAlpha: 1, duration: 3 }, '<')
-    .to('#byline-text', { autoAlpha: 1, duration: 3 }, '<')
-    .to('#byline-text-intro', { autoAlpha: 0.5, duration: 3 }, '<')
+  if (!hasVisited) {
+    gsap.timeline()
+      .fromTo(
+        '#logo-species path',
+        { opacity: 0, x: -700 },
+        { opacity: 0.5, duration: 0.7, stagger: 0.7, x: 0 },
+        '+=1'
+      )
+      .fromTo('#logo-words', { opacity: 0 }, { opacity: 0.5, duration: 3 }, '+=1')
+      .to('#byline-text-intro', { autoAlpha: 1, duration: 3 }, '+=0.2')
+      .to('#logo-species path', { autoAlpha: 1, duration: 3 })
+      .to('#logo-words', { autoAlpha: 1, duration: 3 }, '<')
+      .to('#byline-text', { autoAlpha: 1, duration: 3 }, '<')
+      .to('#byline-text-intro', { autoAlpha: 0.5, duration: 3 }, '<')
+      .then(()=>sessionStorage.setItem('hasVisitedPage', 'true'))
+  }
+  else {
+    gsap.set(['#logo-species path', '#logo-words', '#byline-text'], { opacity: 1 });
+    gsap.set('#byline-text-intro', { opacity: 0.5 })
+  }  
 }
 
 function animateFish() {
